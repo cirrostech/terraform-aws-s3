372d528788e021e6aa7c998e66a8ef7a0fd32e1a
cirrostech <56857967+cirrostech@users.noreply.github.com>
2022-09-06 01:13:28 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@
 resource "random_uuid" "random_suffix" {}
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "${var.bucket_name}-{random_uuid.random_suffix.result}"
+  bucket = "${var.bucket_name}-${random_uuid.random_suffix.result}"
   tags = var.tags
 }
