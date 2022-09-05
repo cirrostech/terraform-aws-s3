52a3a736df53590f3be25bb57b5b32be49999463
cirrostech <56857967+cirrostech@users.noreply.github.com>
2022-09-06 01:18:54 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "random_string" "random_suffix" {
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "${var.bucket_name}-${random_string.random_suffix.result}"
+  bucket = "${var.bucket_name}-"${random_string.random_suffix.result}
   tags = var.tags
 }
